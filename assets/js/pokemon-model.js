cc0051a883e90009d0899304414ddac80e925a7c
danielsantos271 <85268454+danielsantos271@users.noreply.github.com>
2024-01-23 17:32:29 -0300

    Update pokemon-model.js

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,54 @@
+const pokeApi = {};
 
-class Pokemon {
-    number;
-    name;
-    type;
-    types = [];
-    photo;
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.id;
+    pokemon.name = pokeDetail.name;
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types;
+
+    pokemon.types = types;
+    pokemon.type = type;
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+    return pokemon;
 }
+
+pokeApi.getPokemonDetail = async (pokemon) => {
+    try {
+        const response = await fetch(pokemon.url);
+        const pokeDetail = await response.json();
+        return convertPokeApiDetailToPokemon(pokeDetail);
+    } catch (error) {
+        console.error(`Error fetching details for ${pokemon.name}:`, error);
+        throw error;
+    }
+};
+
+pokeApi.getPokemons = async (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    try {
+        const response = await fetch(url, {
+            headers: {
+                'Accept': 'application/json',
+            },
+        });
+
+        const jsonBody = await response.json();
+        const pokemons = jsonBody.results;
+        const detailRequests = pokemons.map(pokeApi.getPokemonDetail);
+        const pokemonsDetails = await Promise.all(detailRequests);
+
+        return pokemonsDetails;
+    } catch (error) {
+        console.error('Error fetching Pokemon list:', error);
+        throw error;
+    }
+};
+
+// Example of usage:
+// const pokemons = await pokeApi.getPokemons();
+// console.log(pokemons);
